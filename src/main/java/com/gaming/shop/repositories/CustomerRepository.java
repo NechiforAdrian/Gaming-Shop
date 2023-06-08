d799fdc51d3c83d0f1563f1cba8713078c612fa8
NechiforAdrian <nechifor.adrian96@yahoo.com>
2023-06-08 10:16:55 +0300

    create properties in dtos/entities,working on CustomerService,repositories  create first api post in controller

--- src/main/java/com/gaming/shop/repositories/CustomerRepository.java
+++ src/main/java/com/gaming/shop/repositories/CustomerRepository.java
@@ -1,4 +1,9 @@
 package com.gaming.shop.repositories;
 
-public interface CustomerRepository {
+import com.gaming.shop.models.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
--- src/main/java/com/gaming/shop/repositories/ProductRepository.java
+++ src/main/java/com/gaming/shop/repositories/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.gaming.shop.repositories;
 
-public interface ProductRepository {
+import com.gaming.shop.models.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
